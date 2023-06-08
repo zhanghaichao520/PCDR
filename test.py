9c677ca6b5ac0b4e54018668841e4302a0d2e929
haichao.zhang <haichao.zhang22@student.xjtlu.edu.cn>
2023-06-08 20:24:05 +0800

    add evel metrics - IOU

--- test.py
+++ test.py
@@ -200,7 +200,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", "-m", type=str, default="DMCB", help="name of models")
     parser.add_argument(
-        "--dataset", "-d", type=str, default="ml-1m", help="name of datasets"
+        "--dataset", "-d", type=str, default="ml-100k", help="name of datasets"
     )
     parser.add_argument("--config_files", type=str, default=None, help="config files")
     parser.add_argument(
