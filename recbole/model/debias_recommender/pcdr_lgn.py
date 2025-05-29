fe8e9b3e3c026678fb04c26e95ca2ec37d685438
张海超 <zhang_haichao@163.com>
2025-05-29 21:42:41 +0800

    Update pcdr_lgn.py

--- recbole/model/debias_recommender/pcdr_lgn.py
+++ recbole/model/debias_recommender/pcdr_lgn.py
@@ -32,8 +32,12 @@ class PCDR_LGN(DebiasedRecommender):
         super(PCDR_LGN, self).__init__(config, dataset)
         # load parameters info
         self.embedding_size = 64
-        self.weight0 = 1
-        self.weight1 = 0.5
+        self.weight0 = 2 # ML-1M
+        # self.weight0 = 3 # Amazon-luxury-beauty-18
+        # self.weight0 = 1 # Netflix
+        self.weight1 = 0.5 # ML-1M
+        # self.weight1 = 1 # Amazon-luxury-beauty-18
+        # self.weight1 = 0.3 # Netflix
         self.weight2 = 2
         self.weight3 = 2
         # define layers and loss
